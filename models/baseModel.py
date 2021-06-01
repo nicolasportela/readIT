19111ea5715dc0f962ef0f55e14fa0abe9e77b91
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-06-01 11:19:13 -0300

    update str method

--- models/baseModel.py
+++ models/baseModel.py
@@ -5,19 +5,14 @@ import models
 from uuid import uuid4
 from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
+from engine.dbStorage import DBStorage
 
 Base = declarative_base()
 
 class BaseModel:
     """Class that defines all common attributes/methods
     for other classes will inherit"""
-    # def __init__(self, *args):
-    """Initialization of the base model"""
-    # self.id = str(uuid4())
-    # for element in args:
-    #    for key in element:
-    #        print(element[key])
-    
+
     """
     In memorium our first user:
     
@@ -27,17 +22,16 @@ class BaseModel:
     self.Password = "ABC1234"
     self.City = "Paradise"
     """
-    # ¿revisar cómo visualizaremos los objetos?
-    # def __str__(self):
-    #    """String representation of the BaseModel class"""
-    #    return "[{:s}] ({:s}) {}".format(self.__class__.__name__, self.id,
-    #                                     self.__dict__)
 
-    # ¿cómo actúa esto en los métodos de storage?
+    def __str__(self):
+        """String representation of the BaseModel class"""
+        Id = 'Id' + self.__class__.__name__
+        return "[{:s}] ({:s}) {}".format(self.__class__.__name__, eval('self.{}'.format(Id)),
+                                         self.__dict__)
     def save(self):
         """Create new register in the db"""
-        models.storage.new(self)
-        models.storage.save()
+        DBStorage.new(self)
+        DBStorage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of the instance"""
