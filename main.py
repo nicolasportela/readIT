8cdae1b0409064983f7f4855ce9d82dbc333eea7
Roberto Ribeiro <rmribeiro.uy@gmail.com>
2021-06-02 13:37:19 -0300

    new main

--- main.py
+++ main.py
@@ -4,17 +4,30 @@ from models.baseModel import BaseModel
 from models.users import User
 from models.books import Book
 
-prueba_user = {'FirstName':'Eva', 'LastName':'DaughterOfGod', 'Mail':'contacto@readIT', 'Phone':'099111222', 'Password':'Hola', 'City':'Paradise'} 
-prueba_book = {'Authors':'George Orwell', 'Title':'1984', 'Description':'Love this novel!', 'Status':'Requested'}
-prueba1 = User(prueba_user)
-prueba2 = Book(prueba_book)
-print(prueba1)
-print(prueba2)
+print("Create test dictonaries...")
+eva = {'FirstName':'Eva', 'LastName':'DaughterOfGod', 'Mail':'evacontacto@readIT', 'Phone':'099111222', 'Password':'Hola', 'City':'Paradise'}
+adan = {'FirstName':'Adan', 'LastName':'Son ff God', 'Mail':'adancontacto@readIT', 'Phone':'099111222', 'Password':'Hola', 'City':'Paradise'} 
+1984 = {'Authors':'George Orwell', 'Title':'1984', 'Description':'Love this novel!', 'Status':'Requested'}
 
+Print("Create instants User...")
+user1 = User(eva)
+user2 = User(adan)
+
+print("Create instants Book...")
+book1 = Book(1984)
+
+print("Show representations...")
+
+print(user1)
+print(user2)
+print(book1)
+
+Print("Adding to mysql...")
 db = DBStorage()
 db.reload()
 db.new(prueba1)
 db.new(prueba2)
 db.save()
 db.reload()
+print("Printing all objects...")
 db.all()
